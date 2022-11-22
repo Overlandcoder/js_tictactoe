888adb08324d7d5f925279310d42fda37efbef90
Overlander <anuragnandal@hotmail.com>
2022-11-21 21:33:23 -0500

    Begin adding Display and Game modules

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const Player = (name, symbol) => {
 const displaySymbols = (() => {
   for (i = 0; i <= 8; i++) {
     let cell = document.getElementById(i);
-    console.log(i);
     let symbol = Gameboard.boardArray[i];
     cell.textContent = symbol;
   }
@@ -20,7 +19,31 @@ Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
 }
 
-const player1 = Player("Player1");
-const player2 = Player("Player2");
-player1.symbol = ["X", "O"].random();
-player2.symbol = player1.symbol === "X" ? "O" : "X";
+const Display = (function() {
+  const infoDiv = document.querySelector(".info");
+  infoDiv.textContent = "test";
+})();
+
+const Game = (function() {
+  const player1 = Player("Player1");
+  const player2 = Player("Player2");
+  player1.symbol = ["X", "O"].random();
+  player2.symbol = player1.symbol === "X" ? "O" : "X";
+  let currentPlayer = player1;
+
+  function play() {
+    let roundsPlayed = Gameboard.boardArray.length
+
+    for (i = roundsPlayed; roundsPlayed <= 9; i++) {
+      currentPlayer = switchTurn();
+    }
+  }
+
+  function switchTurn() {
+    return currentPlayer == player1 ? player2 : player1;
+  }
+
+  return { play };
+})();
+
+Game.play();
