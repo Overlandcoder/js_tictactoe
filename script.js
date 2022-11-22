f045788a5067cf144dae1291b37b81af87317d93
Overlander <anuragnandal@hotmail.com>
2022-11-22 08:15:55 -0500

    Add Play Game button

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ const Game = (function() {
   player2.symbol = player1.symbol === "X" ? "O" : "X";
   let currentPlayer = player1;
   const cells = document.querySelectorAll(".cell");
+  const playButton = document.querySelector(".play");
   const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                             [0, 3, 6], [1, 4, 7], [2, 5, 8],
                             [0, 4, 8], [2, 4, 6]];
@@ -107,7 +108,5 @@ const Game = (function() {
     }
   }
 
-  return { play };
+  playButton.addEventListener("click", play)
 })();
-
-Game.play();
