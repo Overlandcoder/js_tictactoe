198168239572e8d1d34f3907dd088cc07a7c7345
Overlander <anuragnandal@hotmail.com>
2022-11-21 20:36:02 -0500

    Create function inside IIFE to display symbols in cells

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const Gameboard = (function() {
-  let boardArray = [];
+  let boardArray = ["x", "o", "x", "x", "x", "o", "x", "x", "o"];
   return { boardArray };
 })();
 
 const Player = (name, symbol) => {
   return { name, symbol };
 };
+
+const displaySymbols = (() => {
+  for (i = 0; i <= 8; i++) {
+    let cell = document.getElementById(i);
+    console.log(i);
+    let symbol = Gameboard.boardArray[i];
+    cell.textContent = symbol;
+  }
+})();
