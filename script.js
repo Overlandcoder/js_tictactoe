cf98823eb90a71aaec58df290fbbad32dff4c7a9
Overlander <anuragnandal@hotmail.com>
2022-11-23 19:44:20 -0500

    Use Prettier to improve code style

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const Gameboard = (function() {
+const Gameboard = (function () {
   let boardArray = [];
   const full = () => boardArray.length === 9 && allCellsFilled();
   const clearBoard = () => boardArray.splice(0, boardArray.length);
-  const markCell = (index, symbol) => boardArray[index] = symbol;
-  const symbolAt = index => boardArray[index];
+  const markCell = (index, symbol) => (boardArray[index] = symbol);
+  const symbolAt = (index) => boardArray[index];
 
   function allCellsFilled() {
     for (i = 0; i <= 8; i++) {
@@ -20,25 +20,29 @@ const Player = (name, symbol) => {
 };
 
 Array.prototype.random = function () {
-  return this[Math.floor((Math.random()*this.length))];
-}
+  return this[Math.floor(Math.random() * this.length)];
+};
 
-const Display = (function() {
+const Display = (function () {
   const infoDiv = document.querySelector(".info");
-  const notifyTurn = name => infoDiv.textContent = `${name}, it's your turn.`;
-  const announceTie = () => infoDiv.textContent = "Tie game.";
+  const notifyTurn = (name) =>
+    (infoDiv.textContent = `${name}, it's your turn.`);
+  const announceTie = () => (infoDiv.textContent = "Tie game.");
   const resetInfoDiv = () => infoDiv.classList.remove("win-text");
   const cells = document.querySelectorAll(".cell");
-  cells.forEach(cell => cell.addEventListener("click", () => Game.playRound(cell)));
-  const clearButtons = () => cells.forEach(cell => cell.textContent = "");
-  const enableCells = () => cells.forEach(cell => cell.disabled = false);
-  const disableAllCells = () => cells.forEach(cell => disableCell(cell));
-  const changeCursor = () => cells.forEach(cell => cell.style.cursor = "pointer");
+  cells.forEach((cell) =>
+    cell.addEventListener("click", () => Game.playRound(cell))
+  );
+  const clearButtons = () => cells.forEach((cell) => (cell.textContent = ""));
+  const enableCells = () => cells.forEach((cell) => (cell.disabled = false));
+  const disableAllCells = () => cells.forEach((cell) => disableCell(cell));
+  const changeCursor = () =>
+    cells.forEach((cell) => (cell.style.cursor = "pointer"));
 
-  const announceWinner = name => {
+  const announceWinner = (name) => {
     infoDiv.textContent = `${name} has won!`;
     infoDiv.classList.add("win-text");
-  }
+  };
 
   function notifySymbols(p1Symbol, p2Symbol) {
     const symbolsDiv = document.querySelector(".symbols");
@@ -51,20 +55,37 @@ const Display = (function() {
     cell.style.cursor = "auto";
   }
 
-  return { notifyTurn, notifySymbols, announceWinner, announceTie, resetInfoDiv,
-           clearButtons, enableCells, disableAllCells, disableCell, changeCursor };
+  return {
+    notifyTurn,
+    notifySymbols,
+    announceWinner,
+    announceTie,
+    resetInfoDiv,
+    clearButtons,
+    enableCells,
+    disableAllCells,
+    disableCell,
+    changeCursor,
+  };
 })();
 
-const Game = (function() {
+const Game = (function () {
   const player1 = Player("Player 1");
   const player2 = Player("Player 2");
   player1.symbol = ["X", "O"].random();
   player2.symbol = player1.symbol === "X" ? "O" : "X";
   let currentPlayer;
   const playButton = document.querySelector(".play");
-  const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                         [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                         [0, 4, 8], [2, 4, 6]];
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   let playing = false;
 
   function play() {
@@ -96,11 +117,11 @@ const Game = (function() {
     Display.notifyTurn(currentPlayer.name);
   }
 
-  const switchTurn = () => currentPlayer === player1 ? player2 : player1;
-  const gameWon = () => winningCombos.some(combo => allSameSymbol(combo));
-  
+  const switchTurn = () => (currentPlayer === player1 ? player2 : player1);
+  const gameWon = () => winningCombos.some((combo) => allSameSymbol(combo));
+
   function allSameSymbol(combo) {
-    return combo.every(i => Gameboard.symbolAt(i) === currentPlayer.symbol);
+    return combo.every((i) => Gameboard.symbolAt(i) === currentPlayer.symbol);
   }
 
   function gameOver() {
